3db6f74c725ee6acaa490dead65f2d1daf305a38
aibbya <aibbyale@gmail.com>
2019-05-23 21:03:12 -0300

    Class jueves 23.5.19

--- boo.js
+++ boo.js
@@ -1,8 +1,38 @@
+if (document.getElementById("senate_data")){
+  var pagUrl = 'https://api.propublica.org/congress/v1/113/senate/members.json'
+} else {
+ var pagUrl = 'https://api.propublica.org/congress/v1/113/house/members.json'
+};
 
-var app = new Vue ({
+var app = new Vue({
   el: '#app',
   data: {
-    probando : "hola mundo BOOOO",
+    numberOfDemocrats: 0,
+    numberOfIndependents: 0,
+    numberOfRepublicans: 0,
+    total: 0,
+    democratsVotes: 0,
+    republicansVotes: 0,
+    independentsVotes: 0,
+    total_average: 0,
+    leastAttendance: [],
+    mostAttendance: [],
+    least_loyal: [],
+    most_loyal: [],
+
+  },
+  methods: {
+    traeData: (function () {
+      fetch(pagUrl, {
+        headers: {
+          'X-API-Key': 'D2sQEk1LttT9w8Vydx7vfZZtD3Cag10zupr6TxbL'
+        }
+      }).then(function (respuesta) {
+        return respuesta.json()
+      }).then(function (datos) {
+        console.table(datos.results[0].members)
+      })
+    })(),
 
   }
 })
